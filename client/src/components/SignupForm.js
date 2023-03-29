77c6a6852af635e2575dfb408ed905561afac235
github_kevoconn <kcoconnor84@gmail.com>
2023-03-29 13:46:23 -0500

    edited index and signupform to allow users to sign up

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Form, Button, Alert } from "react-bootstrap";
+import { ADD_USER } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import { createUser } from "../utils/API";
+import Auth from "../utils/auth";
 
 const SignupForm = () => {
   // set initial form state
-  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ username: "", email: "", password: "" });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,18 +32,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      // Execute mutation and pass in defined parameter data as variables
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      console.log(data);
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
-      setShowAlert(true);
     }
 
     setUserFormData({
